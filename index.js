3172ff4ab7a59e57405b14212a50c81c2b713fef
nhamonin <nazarii.hamonin@gmail.com>
2023-06-22 10:44:48 +0300

    Rename folder from 'dataClasses to 'data' and update imports

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import Faceit from './classes/Faceit.js';
-import Championships from './classes/dataClasses/Championships.js';
-import Games from './classes/dataClasses/Games.js';
-import Hubs from './classes/dataClasses/Hubs.js';
-import Leaderboards from './classes/dataClasses/Leaderboards.js';
-import Matches from './classes/dataClasses/Matches.js';
-import Organizers from './classes/dataClasses/Organizers.js';
-import Players from './classes/dataClasses/Players.js';
-import Rankings from './classes/dataClasses/Rankings.js';
-import Search from './classes/dataClasses/Search.js';
-import Teams from './classes/dataClasses/Teams.js';
-import Tournaments from './classes/dataClasses/Tournaments.js';
+import Championships from './classes/data/Championships.js';
+import Games from './classes/data/Games.js';
+import Hubs from './classes/data/Hubs.js';
+import Leaderboards from './classes/data/Leaderboards.js';
+import Matches from './classes/data/Matches.js';
+import Organizers from './classes/data/Organizers.js';
+import Players from './classes/data/Players.js';
+import Rankings from './classes/data/Rankings.js';
+import Search from './classes/data/Search.js';
+import Teams from './classes/data/Teams.js';
+import Tournaments from './classes/data/Tournaments.js';
 
 export {
   Faceit,
